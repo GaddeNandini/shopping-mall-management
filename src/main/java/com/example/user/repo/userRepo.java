6c6bba7d95b51313a972e82d9f8ff3038b146d8d
NANDINI <NANDINI@LAPTOP-3U3GA0UO>
2025-09-13 17:06:58 +0530

    Shopping Mall Management Project

--- src/main/java/com/example/user/repo/userRepo.java
+++ src/main/java/com/example/user/repo/userRepo.java
@@ -3,6 +3,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.user.userdetails;
 
-public interface userRepo  extends JpaRepository<userdetails, Long>{
+public interface userRepo  extends JpaRepository<userdetails, Integer>{
 
 }
